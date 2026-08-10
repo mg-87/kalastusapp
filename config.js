19d932af9bdf9799f5a8e0b258965b303f273fd1
mg-87 <mattsson.gaming@gmail.com>
2026-08-10 03:47:11 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const APP_CONFIG = {
+    DRIVE_WEB_APP_URL: "https://script.google.com/macros/s/AKfycby2mfKjTkJah_QPuR9ypIfopxYABYdDR1LmcgLhfUr0-_y2ihsgA11L-jZB0BrFIJZ3yg/exec",
+    PASSWORD: "Kala1.!"
+};
