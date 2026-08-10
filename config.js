1c49e0d3346cd996d6ba1da96e8d6ee1801fe9cd
mg-87 <mattsson.gaming@gmail.com>
2026-08-10 04:28:39 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const APP_CONFIG = {
-    DRIVE_WEB_APP_URL: "https://script.google.com/macros/s/AKfycby2mfKjTkJah_QPuR9ypIfopxYABYdDR1LmcgLhfUr0-_y2ihsgA11L-jZB0BrFIJZ3yg/exec"
+    DRIVE_WEB_APP_URL: "https://script.google.com/macros/s/AKfycby-X2U5pfQv9EhJ-NMk150hs3KMWzfRXt4bVqeWMswkM__k_9qbKOFdknXPzzCMqNciYA/exec"
 };
